964aa6048d28c1cf50aba3ef201cfafb025e55a9
apoff <apofficial2002@gmail.com>
2023-12-03 11:12:10 +0530

    validations and APIView completed

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -2,10 +2,16 @@ from rest_framework import serializers
 from watchlist_app.models import Movie
 
 
+def description_length(value):
+    if len(value) > 10:
+        raise serializers.ValidationError("Description is too long")
+    return value
+
+
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField()
-    description = serializers.CharField()
+    description = serializers.CharField(validators=[description_length])
     active = serializers.BooleanField()
 
     def create(self, validated_data):
@@ -17,3 +23,14 @@ class MovieSerializer(serializers.Serializer):
         instance.active = validated_data.get('active', instance.active)
         instance.save()
         return instance
+
+    def validate_name(self, value):
+        if len(value) < 2:
+            raise serializers.ValidationError("Name is too short")
+        else:
+            return value
+
+    def validate(self, data):
+        if data['name'] == data['description']:
+            return serializers.ValidationError("Tittle and Description should be different")
+        return data
