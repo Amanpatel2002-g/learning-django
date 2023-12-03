22a1f8467cf9f91ed36dc3dc35961194631c9904
apoff <apofficial2002@gmail.com>
2023-12-03 12:02:27 +0530

    model.modelSeriealizers completed

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -2,27 +2,10 @@ from rest_framework import serializers
 from watchlist_app.models import Movie
 
 
-def description_length(value):
-    if len(value) > 10:
-        raise serializers.ValidationError("Description is too long")
-    return value
-
-
-class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
-    description = serializers.CharField(validators=[description_length])
-    active = serializers.BooleanField()
-
-    def create(self, validated_data):
-        return Movie.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.description = validated_data.get('description', instance.description)
-        instance.active = validated_data.get('active', instance.active)
-        instance.save()
-        return instance
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = "__all__"
 
     def validate_name(self, value):
         if len(value) < 2:
@@ -34,3 +17,36 @@ class MovieSerializer(serializers.Serializer):
         if data['name'] == data['description']:
             return serializers.ValidationError("Tittle and Description should be different")
         return data
+
+# def description_length(value):
+#     if len(value) > 10:
+#         raise serializers.ValidationError("Description is too long")
+#     return value
+
+
+# class MovieSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     name = serializers.CharField()
+#     description = serializers.CharField(validators=[description_length])
+#     active = serializers.BooleanField()
+#
+#     def create(self, validated_data):
+#         return Movie.objects.create(**validated_data)
+#
+#     def update(self, instance, validated_data):
+#         instance.name = validated_data.get('name', instance.name)
+#         instance.description = validated_data.get('description', instance.description)
+#         instance.active = validated_data.get('active', instance.active)
+#         instance.save()
+#         return instance
+#
+#     def validate_name(self, value):
+#         if len(value) < 2:
+#             raise serializers.ValidationError("Name is too short")
+#         else:
+#             return value
+#
+#     def validate(self, data):
+#         if data['name'] == data['description']:
+#             return serializers.ValidationError("Tittle and Description should be different")
+#         return data
